bdb046e19eec9a65bce1df1314a11968cb06be8f
ianribeiro01 <ianithaloribeiro@gmail.com>
2018-08-28 15:30:50 -0300

    Separação de áreas

---
+++ SisCEV_Aplicacao/Areas/Tabelas/TabelasAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SisCEV_Aplicacao.Areas.Tabelas
+{
+    public class TabelasAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Tabelas";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Tabelas_default",
+                "Tabelas/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
