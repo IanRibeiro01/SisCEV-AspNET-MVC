323351cffc6fd3a9af61f4edc1b12da508678683
ianribeiro01 <ianithaloribeiro@gmail.com>
2018-09-01 23:44:32 -0300

    Configuração Identity, criação de usuario

---
+++ SisCEV_Aplicacao/DAL/IdentityDbContextAplicacao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using SisCEV_Aplicacao.Areas.Seguranca.Models;
+
+namespace SisCEV_Aplicacao.DAL
+{
+    public class IdentityDbContextAplicacao : IdentityDbContext<Usuario>
+    {
+        public IdentityDbContextAplicacao() : base("IdentityDb") { }
+
+        static IdentityDbContextAplicacao()
+        {
+            Database.SetInitializer<IdentityDbContextAplicacao>
+                (new IdentityDbInit());
+        }
+
+        public static IdentityDbContextAplicacao Create()
+        {
+            return new IdentityDbContextAplicacao();
+        }
+
+    }
+
+    public class IdentityDbInit : DropCreateDatabaseIfModelChanges
+        <IdentityDbContextAplicacao>
+    {
+    }
+
+}
