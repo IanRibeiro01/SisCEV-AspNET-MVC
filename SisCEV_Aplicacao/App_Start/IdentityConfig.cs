ceeb246dd45a7cfeb15a306e316a7a3b29764606
ianribeiro01 <ianithaloribeiro@gmail.com>
2018-09-03 14:37:12 -0300

    Finalização de configuração do Identity

---
+++ SisCEV_Aplicacao/App_Start/IdentityConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using SisCEV_Aplicacao.DAL;
+using SisCEV_Aplicacao.Infraestrutura;
+
+namespace SisCEV_Aplicacao
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<IdentityDbContextAplicacao>(IdentityDbContextAplicacao.Create);
+            app.CreatePerOwinContext<GerenciadorUsuario>(GerenciadorUsuario.Create);
+            app.CreatePerOwinContext<GerenciadorPapel>(GerenciadorPapel.Create);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                //Redireciona 
+                LoginPath = new PathString("/Seguranca/Account/Login"),
+            });
+        }
+
+    }
+}
---
+++ SisCEV_Aplicacao/Infraestrutura/GerenciadorUsuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using SisCEV_Aplicacao.DAL;
+using SisCEV_Aplicacao.Areas.Seguranca.Models;
+
+namespace SisCEV_Aplicacao.Infraestrutura
+{
+    public class GerenciadorUsuario : UserManager<Usuario>
+    {
+        public GerenciadorUsuario(IUserStore<Usuario> store) : base(store) { }
+
+        public static GerenciadorUsuario Create
+            (IdentityFactoryOptions<GerenciadorUsuario> options, IOwinContext context)
+        {
+            IdentityDbContextAplicacao db = context.Get<IdentityDbContextAplicacao>();
+            GerenciadorUsuario manager = new GerenciadorUsuario(new UserStore<Usuario>(db));
+            return manager;
+        }
+    }
+}
